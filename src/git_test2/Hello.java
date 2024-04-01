36d8bebcba8d1876a85b934c5108d04d92c05fda
nmmn1004 <jjj4120@gmail.com>
2024-04-01 13:17:49 +0900

    최초 커밋

---
+++ src/git_test2/Hello.java
@@ -0,0 +1,9 @@
+package git_test2;
+
+public class Hello {
+
+    public void say() {
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/git_test2/Main.java
@@ -0,0 +1,9 @@
+package git_test2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Hello().say();
+    }
+
+}
