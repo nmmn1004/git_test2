8ab5d8516c01b50dd556f7ca12342d5902201f65
nmmn1004 <jjj4120@gmail.com>
2024-04-01 14:19:41 +0900

    Main.java 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -3,8 +3,8 @@ package git_test2;
 public class Main {
 
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.say();
+        Hello hello = new Hello();
+        hello.say();
     }
 
 }
