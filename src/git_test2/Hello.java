81669a285cbc8be27baefb9bdc2e9f742667d853
nmmn1004 <jjj4120@gmail.com>
2024-04-01 14:20:52 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,8 +1,16 @@
 package git_test2;
 
 public class Hello {
-    
-    String message = "안녕하세요";
+
+    String message;
+
+    public Hello() {
+        this("안녕하세요");
+    }
+
+    public Hello(String message) {
+        this.message = message;
+    }
 
     public void say() {
         System.out.println(message);
--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -3,7 +3,7 @@ package git_test2;
 public class Main {
 
     public static void main(String[] args) {
-        Hello h = new Hello();
+        Hello h = new Hello("hello world");
         h.say();
     }
 
